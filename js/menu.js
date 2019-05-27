6d8f4e4e27bbc2bd760dd3dcb9d6ed8848f72c69
DaeYoung Name <wsnam0507@naver.com>
2019-05-27 21:11:58 +0900

    change menu

--- js/menu.js
+++ js/menu.js
@@ -2,3 +2,12 @@ function menuChange(obj, color) {
 	obj.style.backgroundColor=  color;
 
 }
+
+function menuClicked(text) {
+	let infoTag = document.getElementById('info');
+	console.log(infoTag);
+
+	if(infoTag == null) return;
+
+	infoTag.innerHTML = text;
+}
