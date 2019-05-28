b9af2563008bf502d17b7dd80c1ea202b0b82c5f
KCH6937 <48710060+KCH6937@users.noreply.github.com>
2019-05-28 16:51:44 +0900

    kch change

--- js/menu.js
+++ js/menu.js
@@ -1,13 +1,15 @@
 function menuChange(obj, color) {
-	obj.style.backgroundColor=  color;
-
+	obj.style.backgroundColor= color;
 }
 
-function menuClicked(text) {
+function menuClicked(text, isImg) {
 	let infoTag = document.getElementById('info');
 	console.log(infoTag);
-
 	if(infoTag == null) return;
-
 	infoTag.innerHTML = text;
+	
+}
+
+function refresh(obj){
+	location.reload();
 }
