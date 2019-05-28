16c86a490651222f1ab82bed6316d3439f961b38
dynam0507 <skaeodud0507@naver.com>
2019-05-28 17:06:56 +0900

    add img box

--- js/menu.js
+++ js/menu.js
@@ -3,11 +3,21 @@ function menuChange(obj, color) {
 }
 
 function menuClicked(text, isImg) {
-	let infoTag = document.getElementById('info');
-	console.log(infoTag);
-	if(infoTag == null) return;
-	infoTag.innerHTML = text;
-	
+	let content = document.getElementsByClassName('content-flash')[0];
+	if(arguments.length == 1 && content == null) return;
+
+	let childrenTags = content.children;
+
+	childrenTags[0].innerHTML = text; //info tag innerHTML Change
+]
+	if(arguments.length == 2) {
+		let imgSource = arguments[1];
+		let imgBoxTag = childrenTags[1];
+		
+		imgBoxTag.style.display = "block";
+		imgBoxTag.children[0].src = imgSource;
+	}
+
 }
 
 function refresh(obj){
